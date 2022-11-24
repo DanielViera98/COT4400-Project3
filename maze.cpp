6ff31936e18387b86ed0ab2cc60de8fb378f4ff4
HeWhoWalksBefore <dviera@usf.edu>
2022-11-23 20:01:59 -0500

    Working addition and pass to addedge. Addedge doesn't work.

--- maze.cpp
+++ maze.cpp
@@ -17,11 +17,11 @@ class AdjList{
     struct node*;
 };*/
 
-class vertex{
+/*class vertex{
     bool visited = false;
     int value = -1;
-};
-
+};*/
+void addEdge(vector<vector<string>> adj[], vector<string> vertex1, vector<string> vertex2, int location1, int location2);
 int main(){
     ifstream maze;
     maze.open("tiny-maze.txt");
@@ -44,28 +44,45 @@ int main(){
     cout << "Start Values: " << get<0>(end) << " " << get<1>(end) << " " << get<2>(end) << endl;
 
     //6 bit directional, North, East, South, West, Up, Down. 1 means connection, 0 means none
-    vector<string> graph;
+    vector<vector<string>> graph;
+    vector<string> vertex;
     string vertexContents;
     for (int i = 0; i < rows * columns * levels; i++){
         maze >> vertexContents;
-        graph.push_back(vertexContents);
-        cout << vertexContents<< " ";
+        vertex.push_back(vertexContents);
+        graph.push_back(vertex);
+        cout << graph.back().at(0) << " ";
+        if (vertexContents.at(0) == '1')    /*if spider can move north, connect with northern vertex*/
+            addEdge(&graph, graph.back(), graph.at(i-columns), i, i-columns);
+        if (vertexContents.at(3) == '1')    /*if spider can move west, connect with western vertex*/
+            addEdge(&graph, graph.back(), graph.at(i-1), i, i-1);
+        if (vertexContents.at(5) == '1')    /*if spider can move down, connect with lower vertex*/
+            addEdge(&graph, graph.back(), graph.at(i-columns*rows), i, i-columns*rows);
+        vertex.clear();
     }
     cout << endl;
     //display contents of graph
-    /*
+    
     for (int i = 0; i < levels * rows * columns; i++){
         if (i % 3 == 0) cout << endl;
         if (i % 9 == 0) cout << endl;
-        cout << graph.at(i) << " ";
+        cout << graph.at(i).at(0) << " ";
     }
-    */
+    
 
 
     return 0;
 }
 
-void addEdge(vector<int> adj[], int vertex1, int vertex2){
-    adj[vertex1].push_back(vertex2);
-    adj[vertex2].push_back(vertex1);
+void addEdge(vector<vector<string>> adj[], vector<string> vertex1, vector<string> vertex2,
+ int location1, int location2){
+    cout << "(Connecting " << vertex1.at(0) << " to " << vertex2.at(0) << ")" << endl;
+    vertex1.push_back(vertex2.at(0));
+    vertex2.push_back(vertex1.at(0));
+    cout << "Finished 1, v1.0 = " << vertex1.at(0) << " & v1.1 = " << vertex1.at(1);
+    cout << ", v2.0 = " << vertex2.at(0) << " & v2.1 = " << vertex2.at(1) << endl;
+
+    adj[location1].push_back(vertex2);
+    adj[location2].push_back(vertex1);
+    cout << "Finished 2" << endl;
 }
