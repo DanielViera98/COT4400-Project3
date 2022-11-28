dc194c4d1f8f5399795b076239d33ce2903dbc2f
HeWhoWalksBefore <dviera@usf.edu>
2022-11-28 17:00:04 -0500

    New program using vertex class

--- maze.cpp
+++ maze.cpp
@@ -6,22 +6,24 @@
 
 using namespace std;
 
-/*
-class AdjList{
-    int levels, rows, columns;
-    list<int> *adjLists;
 
+class vertex{
     public:
-        AdjList(int v);
-        void addEdge(int src, int dest);
-    struct node*;
-};*/
+        void addContents(string newContent) {contents = newContent;};
+        string getContents() {return contents;};
+        void addConnection(pair<char, int> connection) {connections->push_back(connection);};
+        void printConnections() {
+            for (int i = 0; i < connections->size(); i++)
+                cout << connections->at(i).first << " " << connections->at(i).second << " ";
+        };
+    private:
+        bool visited = false;
+        bool finish = false;
+        string contents = " ";
+        vector<pair<char, int>> connections[1];
 
-/*class vertex{
-    bool visited = false;
-    int value = -1;
-};*/
-void addEdge(vector<vector<string>> adj[], vector<string> vertex1, vector<string> vertex2, int location1, int location2);
+};
+void addEdge(vector<vertex> graph[], int location1, int location2, char direction, char oppositeDir);
 int main(){
     ifstream maze;
     maze.open("tiny-maze.txt");
@@ -32,41 +34,49 @@ int main(){
 
     int levels, rows, columns;
     maze >> levels >> rows >> columns;
-    tuple <int, int, int> start, end;
+//    tuple <int, int, int> start, end;
     int temp1, temp2, temp3;
+    int finish, start = -1;
     maze >> temp1 >> temp2 >> temp3;
-    start = make_tuple(temp1, temp2, temp3);
+//    start = make_tuple(temp1, temp2, temp3);
+    start = temp1 * columns * rows + temp2 * columns + temp3;
     maze >> temp1 >> temp2 >> temp3;
-    end = make_tuple(temp1, temp2, temp3);
+//    end = make_tuple(temp1, temp2, temp3);
+    finish = temp1 * columns * rows + temp2 * columns + temp3;
+
 
     cout << levels << " levels, " << rows << " rows, " << columns << " columns " << endl;
-    cout << "Start Values: " << get<0>(start) << " " << get<1>(start) << " " << get<2>(start) << endl;
-    cout << "Start Values: " << get<0>(end) << " " << get<1>(end) << " " << get<2>(end) << endl;
+    cout << "Start = " << start << endl;
+    cout << "Finish = " << finish << endl;
 
     //6 bit directional, North, East, South, West, Up, Down. 1 means connection, 0 means none
-    vector<vector<string>> graph;
-    vector<string> vertex;
+    int graphSize = rows * columns * levels;
+    vector<vertex> graph[graphSize];
     string vertexContents;
-    for (int i = 0; i < rows * columns * levels; i++){
+    for (int i = 0; i < graphSize; i++){
         maze >> vertexContents;
-        vertex.push_back(vertexContents);
-        graph.push_back(vertex);
-        cout << graph.back().at(0) << " ";
+        vertex V;
+        graph->push_back(V);
+        graph->at(i).addContents(vertexContents);
+        string test = graph->at(i).getContents();
+        cout << test;
+        cout << graph->at(i).getContents() << " ";
         if (vertexContents.at(0) == '1')    /*if spider can move north, connect with northern vertex*/
-            addEdge(&graph, graph.back(), graph.at(i-columns), i, i-columns);
+            addEdge(graph, i, i-columns, 'N', 'S');
         if (vertexContents.at(3) == '1')    /*if spider can move west, connect with western vertex*/
-            addEdge(&graph, graph.back(), graph.at(i-1), i, i-1);
+            addEdge(graph, i, i-1, 'W', 'E');
         if (vertexContents.at(5) == '1')    /*if spider can move down, connect with lower vertex*/
-            addEdge(&graph, graph.back(), graph.at(i-columns*rows), i, i-columns*rows);
-        vertex.clear();
+            addEdge(graph, i, i-columns*rows, 'D', 'U');
     }
-    cout << endl;
+
+    cout << endl << "DISPLAY CONTENTS" << endl;
     //display contents of graph
     
-    for (int i = 0; i < levels * rows * columns; i++){
+    for (int i = 0; i < graphSize; i++){
         if (i % 3 == 0) cout << endl;
         if (i % 9 == 0) cout << endl;
-        cout << graph.at(i).at(0) << " ";
+        cout << graph->at(i).getContents() << " ";
+        graph->at(i).printConnections();
     }
     
 
@@ -74,25 +84,15 @@ int main(){
     return 0;
 }
 
-void addEdge(vector<vector<string>> adj[], vector<string> vertex1, vector<string> vertex2,
- int location1, int location2){
-    cout << "(Connecting " << vertex1.at(0) << " to " << vertex2.at(0) << ")" << endl;
-    vertex1.push_back(vertex2.at(0));
-    vertex2.push_back(vertex1.at(0));
-    cout << "Finished 1, v1.0 = " << vertex1.at(0) << " & v1.1 = " << vertex1.at(1);
-    cout << ", v2.0 = " << vertex2.at(0) << " & v2.1 = " << vertex2.at(1) << endl;
+void addEdge(vector<vertex> graph[], int location1, int location2, char direction, char oppositeDir){
+    cout << "(Connecting " << graph->at(location1).getContents() << " to " << graph->at(location2).getContents() << ")" << endl;
+    pair<char, int> newConnection;
+    newConnection.first = direction;
+    newConnection.second = location2;
+    graph->at(location1).addConnection(newConnection);
 
-    cout << adj->size() << "long, " << location1 << " " << location2 << endl;
-    if (location1 < 0 || location2 < 0){
-        cout << "size 0" << endl;
-        return;
-    }
-    if (location1 >= adj->size()){
-        cout << "Resize" << endl;
-        adj->resize(location1+1);
-    }
-    adj[location1].push_back(vertex2);
-    cout << "Finished 1" << endl;
-    adj[location2].emplace_back(vertex1);
-    cout << "Finished 2" << endl;
+    newConnection.first = oppositeDir;
+    newConnection.second = location1;
+    graph->at(location2).addConnection(newConnection);
 }
+
